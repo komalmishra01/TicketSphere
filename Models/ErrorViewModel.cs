e437d99a21f7e97a9f967891a5c4f285bc433d5b
komalmishra <mishrakomal0108@gmail.com>
2026-03-27 09:46:30 +0530

    Initial project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TicketingSystem_DotNetMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
